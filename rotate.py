07a5bcd51c949a2464e8a4188117c0c0df68df22
Ved-sketch <vedgupta1806@gmail.com>
2025-08-03 16:12:29 +0530

    Added the initial rotate function file

---
+++ rotate.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import io
+
+def rotate(image_bytes: bytes, degrees: int = 90) -> bytes:
+
+    try:
+        # Use a BytesIO object to treat the bytes as a file.
+        with io.BytesIO(image_bytes) as byte_stream:
+            img = Image.open(byte_stream)
+
+            # Storing the original format.
+            original_format = img.format
+
+            # Rotate the image.
+            rotated_img = img.rotate(degrees, expand=True)
+
+            # Create a new BytesIO object to save the rotated image.
+            rotated_byte_stream = io.BytesIO()
+
+            # Save the rotated image
+            rotated_img.save(rotated_byte_stream, format=original_format)
+            return rotated_byte_stream.getvalue()
+
+    except Exception as e:
+        print(f"An error occurred during image rotation: {e}")
+        return b''
